6c6543f93bdf835d8c4a9d9370ccb7440a271f45
Hung Le <hungle913@gmail.com>
2020-04-23 13:53:48 -0700

    Fixed schema file to reflect burgers. Created seeds file with 3 burger entries for db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Hamburger", True);
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Chicken burger", True);
