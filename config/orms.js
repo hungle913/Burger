af3054179c23a82d0ea4f2ce66297e80f054e370
Hung Le <hungle913@gmail.com>
2020-04-26 13:07:11 -0700

    added ORM to pull all data from database

--- config/orms.js
+++ config/orms.js
@@ -1,3 +1,11 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
+var orm = {
+    all: function(userInput, cb) {
+        connection.query('SElECT * FROM ' + userInput + ';', function(err, result){
+            if(err) throw err;
+            cb(result)
+        })
+    }
+}
