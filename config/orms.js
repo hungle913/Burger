889cd260e0daa694d9695a59e25b2f86f92fbb60
Hung Le <hungle913@gmail.com>
2020-04-28 14:41:19 -0700

    add create function to orms file

--- config/orms.js
+++ config/orms.js
@@ -10,7 +10,14 @@ var orm = {
     },
 
     update: function(tableInput, condition, cb) {
-        connection.query('UPDATE '+tableInput +' SET DEVOURED=true WHERE id='+ condition +';', function(err,result){
+        connection.query('UPDATE '+ tableInput +' SET DEVOURED=true WHERE id='+ condition +';', function(err,result){
+            if(err)throw err;
+            cb(result);
+        })
+    },
+
+    create: function(tableInput, val, cb) {
+        connection.query('INSERT INTO ' + tableInput + '(BURGER_NAME) VALUES (' + val + ');', function(err, result) {
             if(err)throw err;
             cb(result);
         })
