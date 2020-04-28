680a46f6984c384fe640779012cabee416980c8a
Hung Le <hungle913@gmail.com>
2020-04-27 17:50:08 -0700

    changed router.put to router.post in controller file. changed all userInput to tableInput in ORMS file for more clarity.

--- config/orms.js
+++ config/orms.js
@@ -2,15 +2,15 @@
 var connection = require("./connection.js");
 
 var orm = {
-    all: function(userInput, cb) {
-        connection.query('SElECT * FROM ' + userInput + ';', function(err, result){
+    all: function(tableInput, cb) {
+        connection.query('SElECT * FROM ' + tableInput + ';', function(err, result){
             if(err) throw err;
             cb(result)
         })
     },
 
-    update: function(userInput, condition, cb) {
-        connection.query('UPDATE '+ tableInput +' SET devoured=true WHERE id='+ condition +';', function(err,result){
+    update: function(tableInput, condition, cb) {
+        connection.query('UPDATE '+tableInput +' SET DEVOURED=true WHERE id='+ condition +';', function(err,result){
             if(err)throw err;
             cb(result);
         })
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -7,7 +7,13 @@ router.get('/', function(req, res) {
         console.log(burger_data)
         res.render('index',{burger_data});
     })
-
 })
+
+router.post('/burgers/update',function(req,res){
+    burger.update(req.body.burger_id, function(result){
+        console.log(result);
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
