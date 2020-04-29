f3109dec5a4fdd045fecd502cbd87d752ae8952f
Hung Le <hungle913@gmail.com>
2020-04-28 22:02:23 -0700

    added another burger to seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Hamburger", false);
 INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Cheeseburger", false);
 INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Chicken burger", false);
-INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Bacon Cheeseburger", false);
-INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Chicken burger", false);
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Bacon Cheeseburger", True);
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Turkey burger", false);
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Double Cheeseburger", false);
