588d208f79318f797e32b68717249e2b87e290f7
Hung Le <hungle913@gmail.com>
2020-04-29 09:53:31 -0700

    adding connection to database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
 INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Hamburger", false);
 INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Cheeseburger", false);
 INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Chicken burger", false);
-INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Bacon Cheeseburger", True);
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Bacon Cheeseburger", true);
 INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Turkey burger", false);
 INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Double Cheeseburger", false);
+INSERT INTO burgers (BURGER_NAME, DEVOURED) VALUES ("Triple Cheeseburger", false);
